69b0e495e813437f85da036bcee5c1c8b80a14ca
zhipenglin <sunandmoon001@163.com>
2024-11-19 14:37:35 +0800

    添加组件描述

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ const propTypes = createProps((z) => {
   return {
     name: z.string().default('name'), age: z.number().default(10), description: z.string().default('description')
   };
-});
+}, '示例组件');
 
 const BaseExample = (p) => {
   const props = propTypes(p);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { z } from 'zod';
 
 const createProps = (...args) => {
-  const { name, callback } =
+  const { name, callback, description } =
     typeof args[0] === 'string'
       ? {
           name: args[0],
-          callback: args[1]
+          callback: args[1],
+          description: args[2]
         }
       : {
-          callback: args[0]
+          callback: args[0],
+          description: args[1]
         };
-  const schema = z.object(callback(z));
+  const schema = z.object(callback(z)).describe(description || '');
   const output = props => {
     return schema.parse(props);
   };
