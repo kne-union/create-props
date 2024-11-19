6c28fd174928e94cc82af99086549d8ecdf89416
zhipenglin <sunandmoon001@163.com>
2024-11-19 14:14:59 +0800

    发布新版本

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import { z } from 'zod';
 
-const createProps = callback => {
+const createProps = (...args) => {
+  const { name, callback } =
+    typeof args[0] === 'string'
+      ? {
+          name: args[0],
+          callback: args[1]
+        }
+      : {
+          callback: args[0]
+        };
   const schema = z.object(callback(z));
-  return props => {
+  const output = props => {
     return schema.parse(props);
   };
+  output.identifier = name;
+  output.schema = schema;
+  return output;
 };
 
 export default createProps;
